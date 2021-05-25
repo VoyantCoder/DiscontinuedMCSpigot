be51850db5a4302d34477030954197e0a2b03d2c
KvinneKraft <KvinneKraft@protonmail.com>
2021-05-25 22:14:24 +0300

    added cooldowns for commands and messages + command blocker

--- VoyantSMP/DashCommandOverride/src/main/java/com/kvinnekraft/DashCommandOverride.java
+++ VoyantSMP/DashCommandOverride/src/main/java/com/kvinnekraft/DashCommandOverride.java
@@ -7,9 +7,14 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class DashCommandOverride extends JavaPlugin
 {
@@ -37,6 +42,14 @@ public class DashCommandOverride extends JavaPlugin
     private String helpMessage = null;
     private String ruleMessage = null;
 
+    private List<String> blockedCommands = new ArrayList<>();
+
+    private String blockMessage = "";
+    private String cooldMessage = "";
+
+    private int cooldown1 = 100;
+    private int cooldown2 = 100;
+
     private void LoadConfiguration()
     {
         try
@@ -58,6 +71,14 @@ public class DashCommandOverride extends JavaPlugin
 
             for (final String a : config.getStringList("rule-message"))
                 ruleMessage += (Colorize(a) + "\n");
+
+            blockedCommands = config.getStringList("blocked-commands");
+
+            cooldown1 = config.getInt("command-cooldown") * 20;
+            cooldown2 = config.getInt("message-cooldown") * 20;
+
+            cooldMessage = Colorize(config.getString("cooldown-message"));
+            blockMessage = Colorize(config.getString("block-message"));
         }
 
         catch(Exception E)
@@ -72,8 +93,70 @@ public class DashCommandOverride extends JavaPlugin
         @EventHandler
         public final void onPlayerJoin(final PlayerJoinEvent E)
         {
+            E.getPlayer().sendMessage(motdMessage);
+        }
+
+
+        final List<UUID> messageQueue = new ArrayList<>();
+
+        private void addToMessageQueue(final Player p)
+        {
+            if (messageQueue.contains(p.getUniqueId()) || p.isOp())
+            {
+                return;
+            }
+
+            getServer().getScheduler().runTaskLaterAsynchronously
+            (
+                inst,
+
+                () ->
+                {
+                    messageQueue.remove(p.getUniqueId());
+                },
+
+                cooldown2
+            );
+
+            messageQueue.add(p.getUniqueId());
+        }
+
+        @EventHandler
+        public final void onPlayerMessage(final AsyncPlayerChatEvent E)
+        {
             final Player p = E.getPlayer();
-            p.sendMessage(motdMessage);
+
+            if (messageQueue.contains(p.getUniqueId()))
+            {
+                p.sendMessage(cooldMessage);
+                addToMessageQueue(p);
+                E.setCancelled(true);
+            }
+        }
+
+
+        final List<UUID> commandQueue = new ArrayList<>();
+
+        private void addToCommandQueue(final Player p)
+        {
+            if (commandQueue.contains(p.getUniqueId()) || p.isOp())
+            {
+                return;
+            }
+
+            getServer().getScheduler().runTaskLaterAsynchronously
+            (
+                inst,
+
+                () ->
+                {
+                    commandQueue.remove(p.getUniqueId());
+                },
+
+                cooldown1
+            );
+
+            commandQueue.add(p.getUniqueId());
         }
 
         @EventHandler
@@ -82,17 +165,35 @@ public class DashCommandOverride extends JavaPlugin
             final String cmd = E.getMessage().toLowerCase().replace("/", "");
             final Player p = E.getPlayer();
 
-            if (cmd.equals("help") || cmd.equals("ehelp"))
+            if (commandQueue.contains(p.getUniqueId()))
+            {
+                p.sendMessage(cooldMessage);
+                addToCommandQueue(p);
+                E.setCancelled(true);
+            }
+
+            else if (cmd.equals("help") || cmd.equals("ehelp"))
             {
                 p.sendMessage(helpMessage);
+                addToCommandQueue(p);
                 E.setCancelled(true);
             }
 
             else if (cmd.equals("rules") || cmd.equals("rule") || cmd.equals("erules"))
             {
                 p.sendMessage(ruleMessage);
+                addToCommandQueue(p);
                 E.setCancelled(true);
             }
+
+            else if (blockedCommands.contains(cmd) && !p.isOp())
+            {
+                p.sendMessage(blockMessage);
+                addToCommandQueue(p);
+                E.setCancelled(true);
+            }
+
+            addToCommandQueue(p);
         }
     }
 
